a585d040d1a10966d89fc4909c018102a2defed9
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2022-05-28 10:36:04 +0530

    Initiate Plugin: TerrainMagic

---
+++ TerrainMagicDevApp/Plugins/TerrainMagic/Source/TerrainMagic/Private/TerrainMagicModule.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 GDi4K. All Rights Reserved.
+
+#pragma once
+
+#include "TerrainMagicModule.h"
+
+void FTerrainMagicModule::StartupModule()
+{
+}
+
+void FTerrainMagicModule::ShutdownModule()
+{
+}
+
+IMPLEMENT_MODULE(FTerrainMagicModule, TerrainMagic)

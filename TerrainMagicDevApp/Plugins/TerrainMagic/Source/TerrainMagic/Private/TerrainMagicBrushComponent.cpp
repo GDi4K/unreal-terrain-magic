ab22f62ff78371b70da50eab1b6e69c1fd91d8cb
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2022-06-05 21:03:59 +0530

    Get rid of the Brush Manager Requirement.

---
+++ TerrainMagicDevApp/Plugins/TerrainMagic/Source/TerrainMagic/Private/TerrainMagicBrushComponent.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TerrainMagicBrushComponent.h"
+
+
+// Sets default values for this component's properties
+UTerrainMagicBrushComponent::UTerrainMagicBrushComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UTerrainMagicBrushComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UTerrainMagicBrushComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UTerrainMagicBrushComponent::SetScalarParam(const FName Parameter, const float Value)
+{
+	BrushMaterial->SetScalarParameterValue(Parameter, Value);
+}
+
+void UTerrainMagicBrushComponent::SetVectorParam(const FName Parameter, const FVector Value)
+{
+	BrushMaterial->SetVectorParameterValue(Parameter, Value);
+}
+
+void UTerrainMagicBrushComponent::SetTextureParam(const FName Parameter, UTexture* Value)
+{
+	BrushMaterial->SetTextureParameterValue(Parameter, Value);
+}
---
+++ TerrainMagicDevApp/Plugins/TerrainMagic/Source/TerrainMagic/Public/TerrainMagicBrushComponent.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "TerrainMagicBrushComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TERRAINMAGIC_API UTerrainMagicBrushComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UTerrainMagicBrushComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(BlueprintReadWrite)
+	UMaterialInstanceDynamic* BrushMaterial;
+	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	void SetScalarParam(FName Parameter, float Value);
+
+	UFUNCTION(BlueprintCallable)
+	void SetVectorParam(FName Parameter, FVector Value);
+
+	UFUNCTION(BlueprintCallable)
+	void SetTextureParam(FName Parameter, UTexture* Value);
+};

8b420b650f370cf5b40479f7932c38ee4955fce7
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2022-06-20 11:05:01 +0530

    Add Area Noise Mask

--- epic-package.bat
+++ epic-package.bat
@@ -1,3 +1,3 @@
 echo "Epic Packaging ..."
 
-"D:\Programs\Epic Games\UE_%1\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Repos\GDi4K\terrain-magic\TerrainMagicDevApp\Plugins\TerrainMagic\TerrainMagic.uplugin -Package=ToPackage -Rocket
+"C:\Program Files\Epic Games\UE_%1\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=C:\Data\Repos\GDi4K\terrain-magic\TerrainMagicDevApp\Plugins\TerrainMagic\TerrainMagic.uplugin -Package=ToPackage -Rocket

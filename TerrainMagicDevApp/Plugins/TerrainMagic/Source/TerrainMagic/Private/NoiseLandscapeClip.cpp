a4f3ddd5bf19492a97cae168a63bd6ee7cdad201
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2022-08-06 04:51:18 +0530

    Introduce the basic class structure for NoiseLandscapeClips.

---
+++ TerrainMagicDevApp/Plugins/TerrainMagic/Source/TerrainMagic/Private/NoiseLandscapeClip.cpp
@@ -0,0 +1,69 @@
+﻿// Copyright (c) 2022 GDi4K. All Rights Reserved.
+
+#include "NoiseLandscapeClip.h"
+#include "Materials/Material.h"
+
+
+// Sets default values
+ANoiseLandscapeClip::ANoiseLandscapeClip()
+{
+}
+
+UMaterial* ANoiseLandscapeClip::GetSourceMaterial() const
+{
+	const FName MaterialPath = "/TerrainMagic/Core/Materials/M_TM_LandscapeClip_Noise.M_TM_LandscapeClip_Noise";
+	return Cast<UMaterial>(StaticLoadObject(UMaterial::StaticClass(), nullptr, *MaterialPath.ToString()));
+}
+
+TArray<FTerrainMagicMaterialParam> ANoiseLandscapeClip::GetMaterialParams()
+{
+	TArray<FTerrainMagicMaterialParam> MaterialParams;
+	
+	MaterialParams.Push({"Texture", HeightMap});
+	MaterialParams.Push({"HeightMultiplier", static_cast<float>(HeightMultiplier)});
+	MaterialParams.Push({"SelectedBlendMode", static_cast<float>(BlendMode)});
+
+	MaterialParams.Push({"HeightMapInputMin", HeightMapRange.InputMin});
+	MaterialParams.Push({"HeightMapInputMax", HeightMapRange.InputMax});
+	MaterialParams.Push({"HeightMapOutputMin", HeightMapRange.OutputMin});
+	MaterialParams.Push({"HeightMapOutputMax", HeightMapRange.OutputMax});
+
+	MaterialParams.Push({"HeightSaturation", HeightSaturation});
+
+	MaterialParams.Push({"SelectedFadeMode", static_cast<float>(FadeMode)});
+	MaterialParams.Push({"FadeSaturation", FadeSaturation});
+	MaterialParams.Push({"FadeMaskSpan", FadeMaskSpan});
+	
+	return MaterialParams;
+}
+
+int ANoiseLandscapeClip::GetHeightMultiplier() const
+{
+	return HeightMultiplier;
+}
+
+FVector2D ANoiseLandscapeClip::GetClipBaseSize() const
+{
+	return HeightMapBaseSize;
+}
+
+void ANoiseLandscapeClip::SetClipBaseSize(FVector2D BaseSize)
+{
+	HeightMapBaseSize = BaseSize;
+}
+
+bool ANoiseLandscapeClip::IsEnabled() const
+{
+	return bEnabled;
+}
+
+void ANoiseLandscapeClip::SetEnabled(bool bEnabledInput)
+{
+	bEnabled = bEnabledInput;
+}
+
+UTexture* ANoiseLandscapeClip::GetHeightMap() const
+{
+	return HeightMap;
+}
+
---
+++ TerrainMagicDevApp/Plugins/TerrainMagic/Source/TerrainMagic/Public/NoiseLandscapeClip.h
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2022 GDi4K. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LandscapeClip.h"
+#include "GameFramework/Actor.h"
+#include "NoiseLandscapeClip.generated.h"
+
+UCLASS()
+class TERRAINMAGIC_API ANoiseLandscapeClip : public ALandscapeClip
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ANoiseLandscapeClip();
+
+protected:
+	
+
+public:
+	virtual UMaterial* GetSourceMaterial() const override;
+	virtual TArray<FTerrainMagicMaterialParam> GetMaterialParams() override;
+	virtual int GetHeightMultiplier() const override;
+	virtual FVector2D GetClipBaseSize() const override;
+	virtual void SetClipBaseSize(FVector2D BaseSize) override;
+	virtual bool IsEnabled() const override;
+	virtual void SetEnabled(bool bEnabledInput) override;
+	virtual UTexture* GetHeightMap() const override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	bool bEnabled = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	UTexture* HeightMap = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	TEnumAsByte<ELandscapeClipBlendMode> BlendMode = LCB_COPY;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	FTerrainMagicRemap HeightMapRange;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General", meta=(ToolTip="A multiplier that will use with the following HeightMap range output values. This value doesn't scale with the clip actor's scaling factors."))
+	int HeightMultiplier = 32000;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	float HeightSaturation = 1.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(ToolTip="Size of the HeightMap in meters before scaling"),  Category="01-General")
+	FVector2D HeightMapBaseSize = {1000, 1000};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General", meta=(DisplayName="----------------------------------------", EditCondition="false"))
+	bool __Divider = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	TEnumAsByte<ELandscapeClipFadeMode> FadeMode = LCF_NONE;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	float FadeMaskSpan = 1.0;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="01-General")
+	float FadeSaturation = 1.0;
+};

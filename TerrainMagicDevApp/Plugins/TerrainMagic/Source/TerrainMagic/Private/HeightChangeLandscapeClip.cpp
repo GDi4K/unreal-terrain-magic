6628ca5c67ca83ad549b3e4135851a2bdeb23e72
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2022-09-12 12:19:53 +0530

    Add the basic HeightChangeLandscapeClip

---
+++ TerrainMagicDevApp/Plugins/TerrainMagic/Source/TerrainMagic/Private/HeightChangeLandscapeClip.cpp
@@ -0,0 +1,93 @@
+﻿// Copyright (c) 2022 GDi4K. All Rights Reserved.
+
+#include "HeightChangeLandscapeClip.h"
+
+#include "Kismet/KismetMaterialLibrary.h"
+#include "Materials/Material.h"
+#include "Kismet/KismetRenderingLibrary.h"
+
+
+// Sets default values
+AHeightChangeLandscapeClip::AHeightChangeLandscapeClip()
+{
+	RenderTarget = UKismetRenderingLibrary::CreateRenderTarget2D(GetWorld(), 1024, 1024, RTF_RG16f);
+	const FName MaterialPath = "/TerrainMagic/Core/Materials/M_RT_HeighChange_Landscape_Clip.M_RT_HeighChange_Landscape_Clip";
+	UMaterial* SourceMaterial = Cast<UMaterial>(StaticLoadObject(UMaterial::StaticClass(), nullptr, *MaterialPath.ToString()));
+	RenderTargetMaterial = UKismetMaterialLibrary::CreateDynamicMaterialInstance(GetWorld(), SourceMaterial);
+}
+
+UMaterial* AHeightChangeLandscapeClip::GetSourceMaterialForHeight() const
+{
+	const FName MaterialPath = "/TerrainMagic/Core/Materials/M_TM_LandscapeClip_Generic.M_TM_LandscapeClip_Generic";
+	return Cast<UMaterial>(StaticLoadObject(UMaterial::StaticClass(), nullptr, *MaterialPath.ToString()));
+}
+
+TArray<FTerrainMagicMaterialParam> AHeightChangeLandscapeClip::GetMaterialParams()
+{
+	TArray<FTerrainMagicMaterialParam> MaterialParams;
+
+	UKismetRenderingLibrary::ClearRenderTarget2D(GetWorld(), Cast<UTextureRenderTarget2D>(RenderTarget));
+	UKismetRenderingLibrary::DrawMaterialToRenderTarget(GetWorld(), Cast<UTextureRenderTarget2D>(RenderTarget), RenderTargetMaterial);
+	
+	MaterialParams.Push({"Texture", RenderTarget});
+	MaterialParams.Push({"HeightMultiplier", static_cast<float>(HeightMultiplier)});
+	MaterialParams.Push({"SelectedBlendMode", static_cast<float>(BlendMode)});
+
+	MaterialParams.Push({"HeightMapInputMin", HeightMapRange.InputMin});
+	MaterialParams.Push({"HeightMapInputMax", HeightMapRange.InputMax});
+	MaterialParams.Push({"HeightMapOutputMin", HeightMapRange.OutputMin});
+	MaterialParams.Push({"HeightMapOutputMax", HeightMapRange.OutputMax});
+
+	MaterialParams.Push({"HeightSaturation", HeightSaturation});
+
+	MaterialParams.Push({"SelectedFadeMode", static_cast<float>(FadeMode)});
+	MaterialParams.Push({"FadeSaturation", FadeSaturation});
+	MaterialParams.Push({"FadeMaskSpan", FadeMaskSpan});
+	
+	return MaterialParams;
+}
+
+int AHeightChangeLandscapeClip::GetHeightMultiplier() const
+{
+	return HeightMultiplier;
+}
+
+FVector2D AHeightChangeLandscapeClip::GetClipBaseSize() const
+{
+	return HeightMapBaseSize;
+}
+
+void AHeightChangeLandscapeClip::SetClipBaseSize(FVector2D BaseSize)
+{
+	HeightMapBaseSize = BaseSize;
+}
+
+bool AHeightChangeLandscapeClip::IsEnabled() const
+{
+	return bEnabled;
+}
+
+void AHeightChangeLandscapeClip::SetEnabled(bool bEnabledInput)
+{
+	bEnabled = bEnabledInput;
+}
+
+void AHeightChangeLandscapeClip::SetZIndex(int Index)
+{
+	ZIndex = Index;
+}
+
+int AHeightChangeLandscapeClip::GetZIndex() const
+{
+	return ZIndex;
+}
+
+UTexture* AHeightChangeLandscapeClip::GetHeightMap() const
+{
+	return RenderTarget;
+}
+
+TArray<FLandscapeClipPaintLayerSettings> AHeightChangeLandscapeClip::GetPaintLayerSettings() const
+{
+	return PaintLayerSettings;
+}
